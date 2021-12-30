18fe0f585aa31746a5546ddc20e836f839a1237e
Kristina Jovanovska <kristinajovanovska993@gmail.com>
2021-12-30 15:33:23 +0100

    + gif in tkinter ^^

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import sqlite3
 import pandas as pd
 import tkinter as tk
+from agif import ImageLabel
+import re
 
 
 def main():
 
-    con = sqlite3.connect("semos_company_names.db")
+    con = sqlite3.connect("sn2.db")
+    cursor = con.cursor()
 
-    df = pd.read_sql('''SELECT * FROM companies''', con, chunksize=100)
+    df = pd.read_sql('''SELECT * FROM companies''', con, chunksize=1000)
 
     for chunk in df:
         for row in chunk.values:
-            yield row
             # command = ''' UPDATE companies SET company_name_cleaned = ? WHERE name = ? '''
-            # cursor.execute(command, [str(row[1]).title().replace('Limited', '').
-            #                replace('Ltd.', '').replace('Ltd', ''), str(row[1])])
+            # # cursor.execute(command, [str(row[1]).title().replace('Limited', '').
+            # #                replace('Ltd.', '').replace('Ltd', ''), str(row[1])])
+            # # con.commit()
+            #
+            # command2 = '''UPDATE companies SET company_name_cleaned = ? WHERE company_name_cleaned = ?'''
+            # cursor.execute(command2, [re.sub(' +', ' ', str(row[2])), str(row[2])])
             # con.commit()
+            yield row
 
     con.close()
 
@@ -28,7 +35,7 @@ class MyApp(tk.Tk):
 
         self.title('Company Names')
         self.iconbitmap('')
-        self.geometry('500x200')
+        self.geometry('600x700')
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
         self.resizable(False, False)
@@ -66,11 +73,14 @@ class FrameOne(tk.Frame):
         drop.grid(row=4, column=0)
         drop.config(width=40, bg='#03C04A', font=18)
 
+        lbl = ImageLabel(self, bg='#086522')
+        lbl.grid(row=1)
+        lbl.load('mtt.gif')
 
     def sqlite_db(self):
-        con = sqlite3.connect("semos_company_names.db")
+        con = sqlite3.connect("sn2.db")
         cursor = con.cursor()
-        df = pd.read_sql('''SELECT * FROM companies ORDER BY company_name_cleaned ASC ''', con, chunksize=100)
+        df = pd.read_sql('''SELECT * FROM companies ORDER BY company_name_cleaned ASC ''', con, chunksize=1000)
         for chunk in df:
             for row in chunk.values:
                 yield row[2]
@@ -78,6 +88,7 @@ class FrameOne(tk.Frame):
 
 
 if __name__ == '__main__':
+    # main()
     # for row in main():
     #     print(row)
     app = MyApp()
