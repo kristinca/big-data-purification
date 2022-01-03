0fda41b2b5b2480966d714a2e0a9638cd91fbd59
Kristina Jovanovska <72104951+kristinca@users.noreply.github.com>
2022-01-03 14:27:48 +0100

    updated main + comments

--- main.py
+++ main.py
@@ -4,37 +4,48 @@ import tkinter as tk
 import re
 
 
-def main():
-
-    con = sqlite3.connect("sn.db")
-    cursor = con.cursor()
-
-    df = pd.read_sql('''SELECT * FROM companies''', con, chunksize=1000)
-
-    for chunk in df:
-        for row in chunk.values:
-            # command = ''' UPDATE companies SET company_name_cleaned = ? WHERE name = ? '''
-            # # cursor.execute(command, [str(row[1]).title().replace('Limited', '').
-            # #                replace('Ltd.', '').replace('Ltd', ''), str(row[1])])
-            # # con.commit()
+# def main():
+#     con = sqlite3.connect("semos_company_names.db")
+#     cursor = con.cursor()
+#
+#     df = pd.read_sql('''SELECT * FROM companies''', con, chunksize=1000)
+#
+#     for chunk in df:
+#         for row in chunk.values:
+            # 1. clean company names:
             #
+            # 1.1. remove "", limited, ltd, etc.
+            # command = ''' UPDATE companies SET company_name_cleaned = ? WHERE name = ? '''
+            # cursor.execute(command, [str(row[1]).title().replace('Limited', '').
+            #                replace('Ltd.', '').replace('Ltd', '').replace('"', ''), str(row[1])])
+            # con.commit()
+            #
+            # 1.2. remove brackets & text in between
+            # command = ''' UPDATE companies SET company_name_cleaned = ? WHERE company_name_cleaned = ? '''
+            # cursor.execute(command, [re.sub("[\(\[].*?[\)\]]", "", str(row[2])), str(row[2])])
+            # con.commit()
+            #
+            # 1.3. normalize company name
             # command2 = '''UPDATE companies SET company_name_cleaned = ? WHERE company_name_cleaned = ?'''
             # cursor.execute(command2, [re.sub(' +', ' ', str(row[2])), str(row[2])])
             # con.commit()
-            yield row
+            #
+            # 2. yield rows
+            # yield row
 
-    con.close()
+    # con.close()
 
 
+# 3. gui
 class MyApp(tk.Tk):
-    """The app"""
+    """ The app """
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
         self.title('Company Names')
         self.iconbitmap('')
-        self.geometry('600x700')
+        self.geometry('600x250')
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
         self.resizable(False, False)
@@ -53,7 +64,7 @@ class MyApp(tk.Tk):
 
 
 class FrameOne(tk.Frame):
-    """ The only frame this app has """
+    """ The only frame """
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -73,7 +84,7 @@ class FrameOne(tk.Frame):
         drop.config(width=40, bg='#03C04A', font=18)
 
     def sqlite_db(self):
-        con = sqlite3.connect("sn2.db")
+        con = sqlite3.connect("semos_company_names.db")
         cursor = con.cursor()
         df = pd.read_sql('''SELECT * FROM companies ORDER BY company_name_cleaned ASC ''', con, chunksize=1000)
         for chunk in df:
@@ -83,8 +94,13 @@ class FrameOne(tk.Frame):
 
 
 if __name__ == '__main__':
+    # 1. clean company names
     # main()
+
+    # 2. print rows from table companies
     # for row in main():
     #     print(row)
+
+    # 3. gui
     app = MyApp()
     app.mainloop()
