5e52747add46793571db237d5f0d59aeec00d6bb
Kristina Jovanovska <72104951+kristinca@users.noreply.github.com>
2022-01-26 21:40:58 +0100

    + with SQL update queries

--- README.md
+++ README.md
@@ -10,9 +10,10 @@ semos_company_names.db.
 * The class CleanCompanyNames from the script
 `sqlite3_with_pandas.py` presents the big data purification
 in three ways - with the multiprocessing library,
-with the threading library, and without these two libraries. 
-It also shows the time needed to complete each step of 
-the purification, as well as the total time.
+with the threading library, and without these two libraries.
+The database update is done in two ways - with dataframe to sql
+and with SQL update queries.
+This script also shows the time needed to complete the purification.
 
 ## Contact
 Author
@@ -25,7 +26,7 @@ Author
 * Use your own database
 
 ## Requirements
-For this script you need the following libraries and modules:
+For this script you need the following libraries, modules and packages:
 
 1. sqlite3 ([documentation](https://docs.python.org/3/library/sqlite3.html))
 2. threading ([documentation](https://docs.python.org/3/library/threading.html))
@@ -45,24 +46,53 @@ has one table - companies, 3 columns
    \
      1.1. without the multiprocessing library - in `sqlite3_with_pandas.py` 
      uncomment only the following lines 
-    ```
-     ccn = CleanCompanyNames()
-     ccn.run_program()
-     ``` 
-
-    1.2. with the multiprocessing library - in `sqlite3_with_pandas.py`
-    uncomment only the following lines
-    ```
-     ccn = CleanCompanyNames()
-     ccn.with_multiprocessing()
-    ```
-
-    1.3. with the multiprocessing library - in `sqlite3_with_pandas.py`
-     uncomment only the following lines    
-    ```
-     ccn = CleanCompanyNames()
-     ccn.with_threading()
-    ```
+   1. with dataframe to sql
+       ```
+        ccn = CleanCompanyNames()
+        ccn.run_program_df()
+        ``` 
+   2. with SQL update queries
+       ```
+       return df 
+       ```
+      
+       ```
+        ccn = CleanCompanyNames()
+        ccn.run_program_sql()
+        ```
+      
+   1.2. with the multiprocessing package - in `sqlite3_with_pandas.py`
+       uncomment only the following lines
+   1. with dataframe to sql
+      ```
+      ccn = CleanCompanyNames()
+      ccn.with_multiprocessing_df()
+      ```
+   2. with SQL update queries
+      ```
+       return df 
+       ```
+      
+       ```
+       ccn = CleanCompanyNames()
+       ccn.with_multiprocessing_sql()
+       ```
+   1.3. with the threading library - in `sqlite3_with_pandas.py`
+           uncomment only the following lines 
+   1. with dataframe to sql
+       ```
+       ccn = CleanCompanyNames()
+       ccn.with_threading_df()
+       ```
+   2. with SQL update queries
+       ```
+       return df 
+       ```
+      
+       ```
+       ccn = CleanCompanyNames()
+       ccn.with_threading_sql()
+       ```
 2. Print rows from the sqlite3 database - in `sqlite3_with_pandas.py`
      uncomment only the following lines    
     ```
@@ -74,6 +104,12 @@ has one table - companies, 3 columns
 1. Change the database name, table name and column names
 to your own sqlite3 database name, table name and column names.
 2. In the method pd.read_sql() change the chunksize 
-appropriate to the number of rows in your table. 
-3. Use the replace() method for any additional occurrences
+appropriate to the number of rows in your table.
+3. In the methods that use SQL update queries
+change the following line
+    ```
+    n = [i for i in range(0, 25000, 5000)]
+    ```
+    to define the start, end, and chunksize of a single query.
+4. Use the replace() method for any additional occurrences
 of substrings that need to be replaced.
--- sqlite3_with_pandas.py
+++ sqlite3_with_pandas.py
@@ -50,103 +50,122 @@ class CleanCompanyNames:
             .replace({'\(.*': ''}, regex=True) \
             .replace({'\s+': ' '}, regex=True)
 
+
         df['company_name_cleaned'] = df['company_name_cleaned'].str.title()
-        # df.to_sql('companies', self.con, chunksize=1000, if_exists='replace', index=False)
+        # with dataframe to sql
+        df.to_sql('companies', self.con, chunksize=1000, if_exists='replace', index=False)
 
-        return df
+        # with SQL update queries
+        # return df
 
-    def update_sql(self, df1, b, a=0):
+    def update_sql(self, df1):
+        """
+        Update sqlite3 db with SQL update query.
+        """
 
-        for row in df1.values[a:b]:
+        self.connect_to_db()
+        for row in df1:
             command = '''UPDATE companies SET company_name_cleaned = ? WHERE name = ?'''
             cursor = self.con.cursor()
             cursor.execute(command, [row[2], row[1]])
             self.con.commit()
+        self.con.close()
 
-    def run_program(self):
+    def run_program_df(self):
         """
         Purification of big data in sqlite3 database
         without using the multiprocessing nor threading library.
+        With dataframe to sql.
         """
 
-        ddf = self.purify_data()
+        start = time.perf_counter()
+        self.purify_data()
+        finish = time.perf_counter()
+        print(f"\nPurification of big data finished in {round(finish - start, 2)} seconds.\n")
+
+    def run_program_sql(self):
+        """
+        Purification of big data in sqlite3 database
+        without using the multiprocessing nor threading library.
+        With SQL update queries.
+        """
 
         start = time.perf_counter()
-
-        # n = [i for i in range(0, 25000, 5000)]
-        n = [i for i in range(0, 21000, 1000)]
-
-        # n = [i for i in range(0, 20500, 500)]
+        ddf = self.purify_data()
+        n = [i for i in range(0, 25000, 5000)]
         for ind in range(0, len(n)-1):
-            if ind == 0:
-                self.update_sql(ddf, n[1])
-            else:
-                self.update_sql(ddf, n[ind]+1, n[ind+1])
-        self.con.close()
-
+            self.update_sql(ddf.values[n[ind]:n[ind+1]])
         finish = time.perf_counter()
-        print(f'\nPurification of big data finished in {round(finish - start, 2)} seconds.\n')
+        print(f"\nPurification of big data finished in {round(finish - start, 2)} seconds.\n")
 
-    def with_multiprocessing(self):
+    def with_multiprocessing_df(self):
         """
         Purification of big data in sqlite3 database
         using the multiprocessing library.
+        With dataframe to sql.
         """
 
-        ddf = self.purify_data()
-
         start = time.perf_counter()
-
-        n = [i for i in range(0, 21000, 1000)]
-
-        p = dict()
-        for ind in range(0, len(n)-1):
-            if ind == 0:
-                p[ind+1] = Process(target=self.update_sql(ddf, n[1]))
-            else:
-                p[ind+1] = Process(target=self.update_sql(ddf, n[ind]+1, n[ind+1]))
-
-        for k in p.keys():
-            p[k].start()
-            print(f'{k}-th process started\n')
-
-        for k in p.keys():
-            p[k].join()
-            print(f'{k}-th process ended\n')
-
-        self.con.close()
-
+        p = Process(target=self.purify_data)
+        p.start()
+        p.join()
         finish = time.perf_counter()
         print(f'\nPurification of big data with the multiprocessing library '
               f'finished in {round(finish - start, 2)} seconds.\n')
 
-    def with_threading(self):
+    def with_multiprocessing_sql(self):
+        """
+        Purification of big data in sqlite3 database
+        using the multiprocessing library.
+        With SQL update queries.
+        """
+
+        start = time.perf_counter()
+        ddf = self.purify_data()
+        n = [i for i in range(0, 25000, 5000)]
+        p = dict()
+        for ind in range(0, len(n)-1):
+            print(f'{ind+1} process starts\n')
+            p[ind+1] = Process(target=self.update_sql(ddf.values[n[ind]:n[ind+1]]))
+            p[ind+1].start()
+            p[ind+1].join()
+            print(f'{ind+1}. process ended\n')
+        finish = time.perf_counter()
+        print(f'\nPurification of big data with the multiprocessing library '
+              f'finished in {round(finish - start, 2)} seconds.\n')
+
+    def with_threading_df(self):
         """
         Purification of big data in sqlite3 database
         using the threading library.
+        With dataframe to sql.
         """
+        start = time.perf_counter()
+        t = Thread(target=self.purify_data)
+        t.start()
+        t.join()
+        finish = time.perf_counter()
+        print(f'\nPurification of big data with the threading library '
+              f'finished in {round(finish - start, 2)} seconds.\n')
 
+    def with_threading_sql(self):
+        """
+        Purification of big data in sqlite3 database
+        using the threading library.
+        With SQL update queries.
+        """
+        start = time.perf_counter()
         ddf = self.purify_data()
 
-        start = time.perf_counter()
-
-        n = [i for i in range(0, 21000, 1000)]
+        n = [i for i in range(0, 25000, 5000)]
 
         t = dict()
         for ind in range(0, len(n)-1):
-            if ind == 0:
-                t[ind+1] = Thread(target=self.update_sql(ddf, n[1]))
-            else:
-                t[ind+1] = Thread(target=self.update_sql(ddf, n[ind]+1, n[ind+1]))
-
-        for k in t.keys():
-            t[k].start()
-            print(f'{k}-th thread started\n')
-
-        for k in t.keys():
-            t[k].join()
-            print(f'{k}-th thread ended\n')
-
+            print(f'{ind+1} thread starts\n')
+            t[ind+1] = Thread(target=self.update_sql(ddf.values[n[ind]:n[ind+1]]))
+            t[ind+1].start()
+            t[ind+1].join()
+            print(f'{ind+1} thread ended\n')
         finish = time.perf_counter()
         print(f'\nPurification of big data with the threading library '
               f'finished in {round(finish - start, 2)} seconds.\n')
@@ -174,7 +193,10 @@ def main():
     """
 
     # ccn = CleanCompanyNames()
-    # ccn.run_program()
-    # ccn.with_multiprocessing()
-    # ccn.with_threading()
+    # ccn.run_program_df()
+    # ccn.run_program_sql()
+    # ccn.with_multiprocessing_df()
+    # ccn.with_multiprocessing_sql()
+    # ccn.with_threading_df()
+    # ccn.with_threading_sql()
     # ccn.print_rows()
